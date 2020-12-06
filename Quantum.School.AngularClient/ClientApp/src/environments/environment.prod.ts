35f826c62491082a3da7ac86ad13d563eea653fc
Glen Reyes <glen@projectgrey.net>
2020-12-06 12:03:49 +0800

    Updated the client app dependencies due to several Angular build errors when publishing for release.

--- Quantum.School.AngularClient/ClientApp/src/environments/environment.prod.ts
+++ Quantum.School.AngularClient/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
-export const environment = {
-  production: true
+export const environment =
+{
+	production: true,
+
+	appApi:
+	{
+		host: 'https://quantum-school.azurewebsites.net/api',
+		classSchedulesEndpoint: '/class-schedules',
+		subjectsEndpoint: '/subjects',
+		studentsEndpoint: '/students',
+		teachersEndpoint: '/teachers',
+	},
 };
